54d795d27b2e181068cc6a7639270711b7d61abc
Dumaru <robinsonmu@unisabana.edu.co>
2020-04-18 18:43:06 -0500

    3d primitives and player controller

---
+++ Heal Them/Assets/Scripts/Gameplay/PlayerController.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    #region Fields
+    public float moveSpeed = 6;
+
+    private Rigidbody playerRb;
+    private Camera viewCamera;
+    private Vector3 velocity;
+
+    [SerializeField]
+    private GameObject weapon;
+    [SerializeField]
+    private GameObject projectile;
+
+    #endregion
+
+    #region Properties
+
+    #endregion
+
+    #region Methods
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        viewCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        RotateTowardsMouse();
+        velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized * moveSpeed;
+        CheckMouseInput();
+    }
+
+    private void CheckMouseInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            // Shoot a projectile
+            GameObject projectileTemp = Instantiate(projectile, weapon.transform.position, weapon.transform.rotation);
+        }
+    }
+
+    /// <summary>
+    /// Rotares the player game object towards where the mouse position is
+    /// </summary>
+    private void RotateTowardsMouse()
+    {
+        Vector3 mousePos = Input.mousePosition;
+
+        Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
+        // Sets the vector components to get the angle
+        mousePos.x = mousePos.x - objectPos.x;
+        mousePos.y = mousePos.y - objectPos.y;
+
+        // Sets the angle between the player position and the mouse position
+        // angle = Atan(sin/cos) in radians, so it converts that value to degrees
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+        // Finally, applies the rotation to the transform
+        transform.rotation = Quaternion.Euler(new Vector3(0, -angle + 90, 0));
+    }
+
+
+    void FixedUpdate()
+    {
+        playerRb.MovePosition(playerRb.position + velocity * Time.fixedDeltaTime);
+    }
+    #endregion
+}
---
+++ Heal Them/Assets/Scripts/Gameplay/Projectile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField]
+    private float force = 100;
+    private Rigidbody projectileRb;
+    private Timer lifeTimer;
+    private float duration = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lifeTimer = gameObject.AddComponent<Timer>();
+        lifeTimer.Duration = duration;
+        lifeTimer.AddTimerFinishedListener(HandleLifeTimerFinishedEvent);
+        lifeTimer.Run();
+        StartMoving(this.transform.forward);
+    }
+
+    private void HandleLifeTimerFinishedEvent()
+    {
+        Destroy(gameObject);
+    }
+
+    public void StartMoving(Vector3 direction)
+    {
+        projectileRb = GetComponent<Rigidbody>();
+        projectileRb.AddForce(direction * force, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
